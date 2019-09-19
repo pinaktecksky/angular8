c5dfcfa8d0ccc01114361a1877fbf422ee182800
Pinak Patel <pinak.tecksky@gmail.com>
2019-09-19 18:43:34 +0530

    Completed the section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { Hero } from './hero';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pra';
+  title = 'Angular';
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(2, 'Bombasto'),
+    new Hero(3, 'Magneta'),
+    new Hero(4, 'Tornado'),
+  ];
+  myHero = this.heroes[0];
 }
