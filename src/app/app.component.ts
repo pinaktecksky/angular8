638e83b5af2e09923bbef8cb2f0baafd00c6581b
Pinak Patel <pinak.tecksky@gmail.com>
2019-09-28 19:41:25 +0530

    Need to be resumed from : Pipes and change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular8';
+  dob = new Date(1980, 2, 4);
+  toggle = true;
+
+  get format() {
+    return this.toggle ? 'shortDate' : 'fullDate';
+  }
+
+  toggleFormat() {
+  this.toggle = !this.toggle;
+  }
 }
